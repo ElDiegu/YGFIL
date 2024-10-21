4f8b8320ea10baeff8476cc79213a10b973c3d18
ElDiegu <eldieguuu@gmail.com>
2024-10-21 19:51:11 +0200

    Updated BrainConnections Minigame to Use ScriptableObjects

--- YGFIL/Assets/_Project/Gameplay Scripts/Managers/MinigameManagers/Minigame 3/BrainConnectionsManager.cs
+++ YGFIL/Assets/_Project/Gameplay Scripts/Managers/MinigameManagers/Minigame 3/BrainConnectionsManager.cs
@@ -1,18 +1,23 @@
 using System.Collections.Generic;
 using Systems.EventSystem;
+using TMPro;
 using UnityEngine;
 using YGFIL.Events;
+using YGFIL.ScriptableObjects;
 
 namespace YGFIL
 {
-    public class MazeOptionsManager : MonoBehaviour
+    public class BrainConnectionsManager : MonoBehaviour, ISOContainer
     {
+        [SerializeField] private BrainConnectionsOptionSetSO brainConnectionsOptionSet;
+        public ScriptableObject ScriptableObject { get => brainConnectionsOptionSet; set { } }
+        
         private int solvedMazes;
         [SerializeField] private List<GameObject> options;
         [SerializeField] private GameObject mazeZone, optionsZone;
         
         EventBinding<SolvedMazeEvent> solvedMazeEventBinding;
-        
+
         private void OnEnable()
         {
             solvedMazeEventBinding = new EventBinding<SolvedMazeEvent>( _ => solvedMazes++ );
@@ -22,6 +27,11 @@ namespace YGFIL
         private void OnDisable() 
         {
             EventBus<SolvedMazeEvent>.Deregister(solvedMazeEventBinding);
+        }
+        
+        private void Awake()
+        {
+            for (int i = 0; i < options.Count ; i++) options[i].GetComponentInChildren<TextMeshProUGUI>().text = brainConnectionsOptionSet.Options[i].Text;
         }
 
         private void ActivateOptions() 
