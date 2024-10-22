ad60eb1150ae28ef6d0c2f5fa4955944b42f9deb
ElDiegu <eldieguuu@gmail.com>
2024-10-22 20:00:55 +0200

    Implemented Minigame UI Animations
    Animations for BreakingIce and Introductions minigames for entering and exiting the screen along with CasterUI and LoveBar

---
+++ YGFIL/Assets/_Project/UI/UIManager/UIManager.cs
@@ -0,0 +1,56 @@
+using Systems.EventSystem;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace YGFIL.Managers
+{
+    public class UIManager : StaticInstance<UIManager>
+    {
+        [field: SerializeField]
+        public Animator UIAnimator { get; set; }
+        
+        EventBinding<PlayAnimationEvent> playAnimationBinding;
+        
+        private void OnEnable()
+        {
+            playAnimationBinding = new EventBinding<PlayAnimationEvent>(playAnimationEvent => UIAnimator.Play(playAnimationEvent.animationString));
+            EventBus<PlayAnimationEvent>.Register(playAnimationBinding);
+        }
+        
+        private void OnDisable()
+        {
+            EventBus<PlayAnimationEvent>.Deregister(playAnimationBinding);
+        }
+    }
+    
+    public struct PlayAnimationEvent : IEvent 
+    {
+        public string animationString;
+    }
+    
+#if UNITY_EDITOR
+    [CustomEditor(typeof(UIManager))]
+    public class UIManagerEditor : Editor 
+    {
+        public static string animationName;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            animationName = EditorGUILayout.TextField(animationName);
+            
+            if (GUILayout.Button("Play Animation")) 
+            {
+                Debug.Log($"Trying to play {animationName}");
+                
+                EventBus<PlayAnimationEvent>.Raise(new PlayAnimationEvent() 
+                {
+                    animationString = animationName
+                });
+            }
+        }
+    }
+#endif
+}
